63a549df3b149013a12465fc8769d4fe35ee5a2f
Rohit <Rohit@128.237.160.195>
2016-05-05 18:54:19 -0400

    final_comit_server

--- src/ServerInit.java
+++ src/ServerInit.java
@@ -1,10 +1,13 @@
 
-import java.io.BufferedWriter;
+/**
+ * MDT final project server program
+ *
+ * @author Rohit Bandi
+ */
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -12,174 +15,191 @@ import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
+ * ServerInit is class initializes some stylists and start listening for
+ * clients. It's a multi threaded server program, as soon as server accepts a
+ * client it forks and start listening for new client.
  *
- * @author Rohit
  */
-public class ServerInit 
-{
-    public static ArrayList<Stylist> stylists ;
-    public static void main(String args[])
-    {
+public class ServerInit {
+
+    /**
+     * ArrayList of stylists
+     */
+    public static ArrayList<Stylist> stylists;
+
+    public static void main(String args[]) {
+        /**
+         * Initializing some stylists for prototype
+         */
         stylistInitialise();
-        try
-        {
+        try {
             int serverPort = 7896; // the server port
             ServerSocket listenSocket = new ServerSocket(serverPort);
             System.out.println("Let the magic begin");
-            while (true)
-            {
+            while (true) {
                 //waiting for client to connect
                 Socket clientSocket = listenSocket.accept();
                 //creating a Connection object which will crete a thread that will handle client connection
                 Connection c = new Connection(clientSocket);
             }
-        }
-        catch (IOException e) 
-        {
+        } catch (IOException e) {
             System.out.println("Listen socket:" + e.getMessage());
         }
     }
-    public static void stylistInitialise()
-    {
-       Stylist s1 = new Stylist("Rohit","Oakland");
-       Stylist s2 = new Stylist("Akash","Oakland");
-       Stylist s3 = new Stylist("Ahana","SquirrelHill");
-       Stylist s4 = new Stylist("Swaroop","SquirrelHill");
-       Stylist s5 = new Stylist("Alka", "ShadySide");
-       Stylist s6 = new Stylist("Himani","ShadySide");
-       stylists = new ArrayList<>();
-       stylists.add(s1);
-       stylists.add(s2);
-       stylists.add(s3);
-       stylists.add(s4);
-       stylists.add(s5);
-       stylists.add(s6);
+
+    /**
+     * stylistInitialise method creates stylists and add it to stylits array
+     * list.
+     */
+    public static void stylistInitialise() {
+        Stylist s1 = new Stylist("Rohit", "Oakland");
+        Stylist s2 = new Stylist("Akash", "Oakland");
+        Stylist s3 = new Stylist("Ahana", "SquirrelHill");
+        Stylist s4 = new Stylist("Swaroop", "SquirrelHill");
+        Stylist s5 = new Stylist("Alka", "ShadySide");
+        Stylist s6 = new Stylist("Himani", "ShadySide");
+        stylists = new ArrayList<>();
+        stylists.add(s1);
+        stylists.add(s2);
+        stylists.add(s3);
+        stylists.add(s4);
+        stylists.add(s5);
+        stylists.add(s6);
     }
-    public static ArrayList<slotDiscount> getDisconts(String area, int numSlots)
-   {
-       ArrayList<slotDiscount> discounts = new ArrayList<>();
-       float[] slotDiscounts = new float[8];
-       String[] stylist_names = new String[8];
-       for(int i=0;i<8;i++)
-       {
-           slotDiscounts[i] = 0;
-           stylist_names[i] = "None";
-       }
-       ArrayList<Stylist> localStylists = new ArrayList<>();
-       for(Stylist s: stylists)
-       {
-           if(s.getArea().equals(area))
-           {
-               localStylists.add(s);
-           }
-       }
-       for(Stylist s: localStylists)
-       {
-           //int[] timeSlot = s.getTimeSlots();
-           HashMap<Integer,String> freeSlots = s.freeSlots(numSlots);
-           if(freeSlots.isEmpty())
-           {
-               continue;
-           }
-           float bookedSlotsDicount = ((float)(8-freeSlots.size())/8)*5;
-           float durationDiscount = ((float)(numSlots)/8)*20;
-           System.out.println("freeSlots of " +s.getName() + " : " + freeSlots.toString() + " bookedSlotsDicount " + bookedSlotsDicount);
-           for(Integer i: freeSlots.keySet())
-           {
-               float totalDiscount =0;
-               if(freeSlots.get(i).equals("yes"))
-               {
-                   totalDiscount = 5 + bookedSlotsDicount + durationDiscount;
-               }
-               else if(freeSlots.get(i).equals("corner"))
-               {
-                   totalDiscount = 2 + bookedSlotsDicount + durationDiscount;
-               }
-               else
-               {
-                   totalDiscount = bookedSlotsDicount + durationDiscount;
-               }
-               if(slotDiscounts[i]<=totalDiscount)
-               {
-                   slotDiscounts[i] = totalDiscount;
-                   stylist_names[i] = s.getName();
-               }
-           }
-       }
-       for(int i=0;i<8;i++)
-       {
-           slotDiscount temp = new slotDiscount();
-           temp.slot =i;
-           temp.name =stylist_names[i];
-           temp.discount = slotDiscounts[i];
-           discounts.add(temp);
-       }       
-       return discounts;
-   }
-  
+
+    /**
+     * GetDiscouns is the method which calculate slot discounts depending on the
+     * area and number of slots required
+     *
+     * @param area area in which service is requested
+     * @param numSlots number of slots user requested
+     * @return return a array list slotDiscount objects which contain slot
+     * discount and stylist details
+     */
+    public static ArrayList<slotDiscount> getDisconts(String area, int numSlots) {
+        ArrayList<slotDiscount> discounts = new ArrayList<>();
+        float[] slotDiscounts = new float[8];
+        String[] stylist_names = new String[8];
+        for (int i = 0; i < 8; i++) {
+            slotDiscounts[i] = 0;
+            stylist_names[i] = "None";
+        }
+        ArrayList<Stylist> localStylists = new ArrayList<>();
+        //getting stylits of given area
+        for (Stylist s : stylists) {
+            if (s.getArea().equals(area)) {
+                localStylists.add(s);
+            }
+        }
+        /**
+         * For each local stylist compute the discount rates for his free slots
+         * depending on the adjacency and the number of slots booked, Then
+         * getting the maximum slot discount for a slot
+         */
+        for (Stylist s : localStylists) {
+            HashMap<Integer, String> freeSlots = s.freeSlots(numSlots);
+            if (freeSlots.isEmpty()) {
+                continue;
+            }
+            /**
+             * Discount depending on the number of free booked slots of a
+             * stylist We are giving weightage to number of booked slots because
+             * we want to fill time slots of a stylist completly before booking
+             * a new stylist, Sending one stylist to area is more profitable
+             * than sending two stylists to same are for same number of
+             * customers
+             */
+            float bookedSlotsDicount = ((float) (8 - freeSlots.size()) / 8) * 5;
+            /**
+             * More number of slots a user booked more discounts he gets This is
+             * grouping discount in our model
+             */
+            float durationDiscount = ((float) (numSlots) / 8) * 20;
+            //for debugging
+            System.out.println("freeSlots of " + s.getName() + " : " + freeSlots.toString() + " bookedSlotsDicount " + bookedSlotsDicount);
+            /**
+             * depending on the adjancy adding some discount
+             */
+            for (Integer i : freeSlots.keySet()) {
+                float totalDiscount = 0;
+                if (freeSlots.get(i).equals("yes")) {
+                    totalDiscount = 5 + bookedSlotsDicount + durationDiscount;
+                } else if (freeSlots.get(i).equals("corner")) {
+                    totalDiscount = 2 + bookedSlotsDicount + durationDiscount;
+                } else {
+                    totalDiscount = bookedSlotsDicount + durationDiscount;
+                }
+                //if current discount of this stylist is better than existing slot discounts, replace it with this stylist
+                if (slotDiscounts[i] <= totalDiscount) {
+                    slotDiscounts[i] = totalDiscount;
+                    stylist_names[i] = s.getName();
+                }
+            }
+        }
+        //creating discounts arraylist
+        for (int i = 0; i < 8; i++) {
+            slotDiscount temp = new slotDiscount();
+            temp.slot = i;
+            temp.name = stylist_names[i];
+            temp.discount = slotDiscounts[i];
+            discounts.add(temp);
+        }
+        return discounts;
+    }
+
 }
-class slotDiscount
-{
+
+/**
+ * slotDicount class holds details of slots
+ */
+class slotDiscount {
+
     int slot;
     String name;
     float discount;
 }
-class Connection extends Thread
-{
+
+class Connection extends Thread {
+
     DataInputStream in;
     DataOutputStream out;
     Socket clientSocket;
 
-    public Connection(Socket aClientSocket)
-    {
-        try 
-        {
+    public Connection(Socket aClientSocket) {
+        try {
             clientSocket = aClientSocket;
-            in = new DataInputStream( clientSocket.getInputStream());
-            out = new DataOutputStream( clientSocket.getOutputStream());
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
             this.start(); //starting a new thread to handle client connection
-        } 
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Connection:" + e.getMessage());
         }
     }
 
     @Override
-    public void run() 
-    {
+    public void run() {
         String location = "";
         int numSlots = 0;
-        try 
-        {
+        try {
             location = in.readUTF();
             numSlots = in.readInt();
-        }
-        catch (IOException ex)
-        {
-            
+        } catch (IOException ex) {
+
         }
         ArrayList<slotDiscount> discounts = ServerInit.getDisconts(location, numSlots);
         System.out.println("Rohit" + numSlots);
-        for(slotDiscount sd: discounts)
-        {
+        for (slotDiscount sd : discounts) {
             try {
                 out.writeInt(sd.slot);
-                out.writeInt((int)sd.discount);
+                out.writeInt((int) sd.discount);
                 out.writeUTF(sd.name);
             } catch (IOException ex) {
                 Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
             }
-            System.out.println("slot: " + sd.slot + " discount: "+ sd.discount + "name: " + sd.name);
+            System.out.println("slot: " + sd.slot + " discount: " + sd.discount + "name: " + sd.name);
         }
-        
+
         try {
             //waiting for user selection
             //sending it from makebooking servlet
@@ -191,16 +211,13 @@ class Connection extends Thread
             Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public void makeBooking(String name,int slot,int numSlots)
-    {
+
+    public void makeBooking(String name, int slot, int numSlots) {
         System.out.println("@makeBooking: name : " + name + " slot: " + slot + " numSlots: " + numSlots);
-        for(Stylist s : ServerInit.stylists)
-        {
-            if(s.getName().equals(name))
-            {
+        for (Stylist s : ServerInit.stylists) {
+            if (s.getName().equals(name)) {
                 System.out.println("@makeBooking name found");
-                for(int i=slot;i<slot+numSlots;i++)
-                {
+                for (int i = slot; i < slot + numSlots; i++) {
                     s.BookTimeSlot(i);
                 }
                 return;
--- src/Stylist.java
+++ src/Stylist.java
@@ -1,119 +1,143 @@
 
-import java.util.ArrayList;
+/**
+ * MDT final project server program
+ *
+ * @author Rohit Bandi
+ */
 import java.util.HashMap;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
+ * Stylist class contains details of the stylist.
  *
- * @author Rohit
  */
-public class Stylist
-{
-    private String area;
-    private int[] timeSlots;
+public class Stylist {
+
+    /**
+     * Name of the stylist
+     */
     private String name;
-    public Stylist(String name, String area)
-    {
+    /**
+     * Area stylist like to work
+     */
+    private String area;
+    /**
+     * timeSlots is a array which contains availability of a stylist If stylist
+     * is booked for particular time slot, corresponding element in the array is
+     * marked as 1
+     */
+    private int[] timeSlots;
+
+    /**
+     * Constructor of stylist class.
+     *
+     * @param name name of the stylist
+     * @param area area in which stylist choose to work
+     */
+    public Stylist(String name, String area) {
         this.name = name;
         this.area = area;
         timeSlots = new int[8];
     }
-    public int[] getTimeSlots()
-    {
+
+    /**
+     * getTimeSlots give the time slots of the stylist.
+     *
+     * @return timeSlots array
+     */
+    public int[] getTimeSlots() {
         return timeSlots;
     }
-    public String getArea()
-    {
+
+    /**
+     * getArea is getter method for area.
+     *
+     * @return area in which stylist choose to work
+     */
+    public String getArea() {
         return area;
     }
-    public String getName()
-    {
+
+    /**
+     * getName getter method for name.
+     *
+     * @return name of the stylist
+     */
+    public String getName() {
         return name;
     }
-    public void BookTimeSlot(int i)
-    {
+
+    /**
+     * BookTimeSlot method marks the timeSlots array for a given index.
+     *
+     * @param i index of the array we want to mark
+     */
+    public void BookTimeSlot(int i) {
         System.out.println("Booked" + name);
-        timeSlots[i]=1;
+        timeSlots[i] = 1;
     }
-    public int numBookedSlots()
-    {
+
+    /**
+     * numBookedSlots method returns the number of slots stylist got booked
+     *
+     * @return number of slots stylist got booked
+     */
+    public int numBookedSlots() {
         int count = 0;
-        for(int i=0;i<timeSlots.length;i++)
-        {
-            if(timeSlots[i]==1)
-            {
+        for (int i = 0; i < timeSlots.length; i++) {
+            if (timeSlots[i] == 1) {
                 count++;
             }
         }
         return count;
     }
-    public HashMap<Integer,String> freeSlots(int numslots) // check this code
-    {
-        HashMap<Integer,String> freeSlots = new HashMap<Integer,String>();
-        for(int i=0;i<=timeSlots.length-numslots;i++)
-        {
-            if(timeSlots[i]==0)
-            {
+
+    /**
+     * freeSlots checks calculates the free slots of a stylists, and assigns
+     * each slot with its adjacency.
+     *
+     * @param numslots number of slots we want to book
+     * @return HashMap which contains slot and adjacency of the slot.
+     */
+    public HashMap<Integer, String> freeSlots(int numslots) {
+        HashMap<Integer, String> freeSlots = new HashMap<>();
+        for (int i = 0; i <= timeSlots.length - numslots; i++) {
+            if (timeSlots[i] == 0) {
                 //check if there are enough slots
-                int flag_enoughLength=1;
-                for(int j=i;j<i+numslots;j++)
-                {
-                    if(timeSlots[j]==1)
-                    {
+                int flag_enoughLength = 1;
+                for (int j = i; j < i + numslots; j++) {
+                    if (timeSlots[j] == 1) {
                         flag_enoughLength = 0;
                         break;
                     }
                 }
                 //if there are enough slots check if it has adj slots booked
-                if(flag_enoughLength == 1)
-                {
-                   int flag_adj = 0;
-                   /*
-                   if(i == 0 /*|| i+numslots == timeSlots.length)
-                   {
-                       //flag_adj = 1;
-                       flag_adj =2;
-                   }
-                   
-                   else
-                   {
-                    */ 
-                       if(i!=0)
-                       {
-                           if(timeSlots[i-1]==1)
-                           {
-                                flag_adj = 1;
-                           }
-                           
-                       }
-                       if(i+numslots != timeSlots.length)
-                       {
-                           if(timeSlots[i+numslots] ==1)
-                           {
-                               flag_adj = 1;
-                           }
-                       }
-                   //}
-                   if(flag_adj==2)
-                   {
-                       freeSlots.put(i, "corner");
-                   }
-                   else if(flag_adj==1)
-                   {
-                       freeSlots.put(i, "yes");
-                   }
-                   else
-                   {
-                       freeSlots.put(i,"no");
-                   }
+                if (flag_enoughLength == 1) {
+                    int flag_adj = 0;
+                    if (i != 0) {
+                        if (timeSlots[i - 1] == 1) {
+                            flag_adj = 1;
+                        }
+
+                    }
+                    if (i + numslots != timeSlots.length) {
+                        if (timeSlots[i + numslots] == 1) {
+                            flag_adj = 1;
+                        }
+                    }
+                    switch (flag_adj) {
+                        case 2:
+                            freeSlots.put(i, "corner");
+                            break;
+                        case 1:
+                            freeSlots.put(i, "yes");
+                            break;
+                        default:
+                            freeSlots.put(i, "no");
+                            break;
+                    }
                 }
             }
-            
+
         }
         return freeSlots;
     }
